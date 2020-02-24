346e0e447b3897c8a9b6910084becbbc98109c60
oguzhan topal <oguzhan2142@hotmail.com>
2020-02-24 19:22:29 +0300

    virsualizations

--- screen.py
+++ screen.py
@@ -7,24 +7,27 @@ class Screen:
 
     def __init__(self):
         self.frame = Tk()
+        self.frame.configure(bg='gray')
+        self.frame.resizable(width=False, height=False)
         self.directory = None
+        self.frame.title('Youtube & Spotify mp3 Downloader')
 
-        self.frame.title('Youtube mp3 Downloader')
-
-        self.L1 = Label(self.frame, text="URL")
-        self.L1.grid(row=0, column=0, sticky=W + E, ipadx=10, padx=4, pady=10)
+        widgets_font = Font(family="Tahoma", weight='bold', size=15)
+        self.L1 = Label(self.frame, bg='gray', font=widgets_font, text="URL:")
+        self.L1.grid(row=0, column=0, sticky=W + E, padx=20, pady=10)
 
         self.url_field = Entry(self.frame, bd=3, width=40)
-        self.url_field.grid(row=0, column=1, sticky=W, ipadx=10, padx=10, pady=10)
+        self.url_field.grid(row=0, column=1, sticky=W + E, padx=18, pady=10)
 
-        self.B_folder = Button(self.frame, text="Select Folder", width=10, justify=CENTER)
+        self.B_folder = Button(self.frame, font=widgets_font, text="Select Folder", width=10, justify=CENTER)
         self.B_folder.grid(row=1, column=0, padx=20, pady=10, sticky=W + E)
 
-        self.B = Button(self.frame, text="Download", width=20, bg='#54FA9B', justify=CENTER)
+        self.B = Button(self.frame, text="Download", font=widgets_font, width=20, bg='#54FA9B', justify=CENTER)
         self.B.grid(row=1, column=1, pady=10, padx=20, sticky=W + E)
 
-        myFont = Font(family="Times New Roman", size=15)
-        self.E2 = Text(self.frame, state=DISABLED, pady=3, padx=10, font=myFont, background="black", foreground="green")
+        E2_font = Font(family="Times New Roman", size=15)
+        self.E2 = Text(self.frame, state=DISABLED, pady=3, padx=10, font=E2_font, background="black",
+                       foreground="green")
         self.E2.grid(row=2, column=0, columnspan=2, sticky=W + E, ipadx=10, padx=10, pady=10)
 
     def screen_show(self):
