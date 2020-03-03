4c684d1f29a2dacb6b7bafd57887b7d74b918909
oguzhan topal <oguzhan2142@hotmail.com>
2020-03-03 13:03:53 +0300

    selenium replaced requests

--- metadata.py
+++ metadata.py
@@ -36,31 +36,30 @@ def extract_text(string=''):
     return string
 
 
-def get_metadata(song_name='her sey fani'):
-    base_url = 'https://www.discogs.com/search/?q=' + query('her sey fani') + '&type=all'
-    chrome_options = Options()
-    chrome_options.add_argument("--disable-extensions")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--headless")
+def create_metadata_object(song_name='her sey fani'):
+    search_url = 'https://www.discogs.com/search/?q=' + query(song_name) + '&type=release'
+    base_url = 'https://www.discogs.com'
+    print('search_url', search_url)
+    r = requests.get(search_url)
+    base_soup = BeautifulSoup(r.content, 'html.parser')
 
-    if platform.system() == 'Windows':
-        driver = webdriver.Chrome('Driver/Windows/chromedriver.exe', options=chrome_options)
-    else:
-        driver = webdriver.Chrome('Driver/MacOs/chromedriver', options=chrome_options)
-    driver.get(base_url)
-    first_result = driver.find_element_by_class_name('search_result_title')
-    first_result.click()
-    soup = BeautifulSoup(driver.page_source.encode("utf-8"), 'html.parser')
+    cards = base_soup.find_all('div', attrs={'class': 'card'})
+    first_box_href = cards[0].find('a').get('href')
+    song_link = base_url + first_box_href
+    song_page_r = requests.get(song_link)
+    soup = BeautifulSoup(song_page_r.content, 'html.parser')
     pic_span = soup.find('span', attrs={'class': 'thumbnail_center'})
     pic = pic_span.find_next('img')
 
     # Save Image
-    page = requests.get(pic['src'])
-    f_ext = os.path.splitext(pic['src'])[-1]
-    f_name = 'img{}'.format(f_ext)
-    with open(f_name, 'wb') as f:
-        f.write(page.content)
-
+    try:
+        page = requests.get(pic['src'])
+        f_ext = os.path.splitext(pic['src'])[-1]
+        f_name = 'img{}'.format(f_ext)
+        with open(f_name, 'wb') as f:
+            f.write(page.content)
+    except:
+        print('resim alinamadi')
     # get informations
     all_divs = soup.find_all('div', attrs={'class': 'content'})
     texts = []
@@ -68,9 +67,8 @@ def get_metadata(song_name='her sey fani'):
         # Label: # Format:# Country:# Released:# Genre:# Style:
         texts.append(extract_text(div.text))
 
-    metadata = Metadata(label=texts[0], country=texts[2],
-                        realase_date=texts[3], genre=texts[4],
-                        style=texts[5]
-                        )
-
-    driver.quit()
+    metadata_obj = Metadata(label=texts[0], country=texts[2],
+                            realase_date=texts[3], genre=texts[4],
+                            style=texts[5]
+                            )
+    return metadata_obj
