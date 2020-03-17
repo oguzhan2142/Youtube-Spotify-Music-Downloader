d7c7f79b694fc3917265714fbf7df59858c9d0f4
oguzhan topal <oguzhan2142@hotmail.com>
2020-03-17 11:29:26 +0300

    bg color & console bd

--- screen.py
+++ screen.py
@@ -11,7 +11,9 @@ class Screen:
 
     def __init__(self):
         self.frame = Tk()
-        self.frame.configure(bg='gray')
+        bg_color = 'gray30'
+        self.frame.configure(background=bg_color)
+
         self.frame.resizable(width=False, height=False)
         self.directory = utils.give_desktop_path()
         self.frame.title('Youtube & Spotify mp3 Downloader')
@@ -19,10 +21,10 @@ class Screen:
         widgets_font = Font(family="Tahoma", weight='bold', size=15)
         buttons_font = Font(family='Tahoma', size=13)
         # URL Area Part
-        self.url_labelframe = LabelFrame(self.frame, bg='gray', bd=0)
+        self.url_labelframe = LabelFrame(self.frame, bg=bg_color, bd=0)
         self.url_labelframe.grid(row=0, column=1)
 
-        self.L1 = Label(self.url_labelframe, bg='gray', font=widgets_font, text="URL:")
+        self.L1 = Label(self.url_labelframe, bg=bg_color, font=widgets_font, text="URL:")
         self.L1.grid(row=0, column=0, sticky=E, pady=10)
 
         self.url_field = Entry(self.url_labelframe, bd=3, width=40)
@@ -34,11 +36,12 @@ class Screen:
         self.download_btn.grid(row=1, column=1, pady=10, padx=23, sticky=E)
 
         # Buttons Part
-        self.buttons_labelframe = LabelFrame(self.frame, bg='gray', bd=0)
+        self.buttons_labelframe = LabelFrame(self.frame, bg=bg_color, bd=0)
         self.buttons_labelframe.grid(row=0, column=0, rowspan=2, padx=10, pady=4, )
 
         self.delete_icon = PhotoImage(file=r"icon/clear_console.png").subsample(1, 2)
-        self.folder_btn = Button(self.buttons_labelframe, font=buttons_font, text="Select Folder", justify=CENTER, bd=0)
+        self.folder_btn = Button(self.buttons_labelframe, font=buttons_font, text="Select Folder",
+                                 justify=CENTER, bd=0)
         self.folder_btn.grid(row=0, column=0, pady=2, sticky=W + E)
 
         self.open_folder_btn = Button(self.buttons_labelframe, font=buttons_font, text="Open Folder", justify=CENTER,
@@ -49,15 +52,15 @@ class Screen:
         self.clear_console_btn.grid(row=2, column=0, pady=2, sticky=W + E)
 
         # Console Part
-        self.console_frame = LabelFrame(self.frame, bg='black')
-        self.console_frame.grid(row=2, column=0, columnspan=2, sticky=W + E, ipadx=0, padx=10, pady=10)
+        self.console_frame = LabelFrame(self.frame, bd=0)
+        self.console_frame.grid(row=2, column=0, columnspan=2, sticky=W + E, ipadx=0, ipady=0, padx=10, pady=10)
 
-        self.scroll = Scrollbar(master=self.console_frame)
+        self.scroll = Scrollbar(master=self.console_frame, bd=0)
         self.scroll.pack(side=RIGHT, fill=Y)
 
         E2_font = Font(family="Times New Roman", size=15)
-        self.console = Text(self.console_frame, state=DISABLED, pady=3, padx=10, font=E2_font, background="black",
-                            foreground="green", bd=0, yscrollcommand=self.scroll.set)
+        self.console = Text(self.console_frame, bd=0, state=DISABLED, pady=3, padx=10, font=E2_font, bg="black",
+                            fg="dark green", yscrollcommand=self.scroll.set, borderwidth=0, highlightthickness=0)
         self.console.pack()
         self.scroll.config(command=self.console.yview)
 
